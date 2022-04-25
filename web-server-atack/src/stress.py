ef1234a795247d8b5e0fcff27134e1d52280b108
SalasJ07 <ate.salas20@gmail.com>
2022-04-24 22:30:04 -0500

    Stress

--- web-server-atack/src/stress.py
+++ web-server-atack/src/stress.py
@@ -15,7 +15,7 @@ def process_command(argumentos):
         print("Error sintaxis: stress -n <cantidad-hilos> HTTPclient -h <host> [<lista-comandos-a-ejecutar>]")
         sys.exit()
     
-    client = ['HTTPclient']
+    client = ['HTTPClient']
 
     for i in range(1, len_args):
         if (argumentos[i] == "-n"):
@@ -38,10 +38,11 @@ def main():
     (num_threads, lista_comando_cliente) = process_command(argumentos)
     comando_cliente = listToString(lista_comando_cliente)
     print("Numero de Threads: " + str(num_threads))
-    print("Comando HTTPclient: " + comando_cliente)
+    print("Comando HTTPClient: " + comando_cliente)
     
     with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        future = executor.submit(os.system("ARCHIVO" + comando_cliente))
+        for n in range(num_threads):
+            executor.submit(os.system("cargo run --bin " + comando_cliente))
 
 main()
 
